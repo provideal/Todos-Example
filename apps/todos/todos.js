b4e0b8512fe24fbc48402669c878789715ff010e
René Sprotte <rene.sprotte@provideal.net>
2011-04-06 15:49:30 +0200

    Try to connect to Rails backend using a datastore

--- apps/todos/todos.js
+++ apps/todos/todos.js
@@ -4,12 +4,39 @@
 // ==========================================================================
 /*globals Todos */
 
-Todos = SC.Application.create();
+Todos = SC.Application.create({
+  NAMESPACE: 'Todos',
+  VERSION: '0.1.0',
 
-Todos.Todo = SC.Object.extend({
+  store: SC.Store.create().from('Todos.DataSource')
+});
+
+Todos.TodoJSONProxy = SC.Object.create({
+  normalize_json: function(data) {
+    result = new Array();
+    if (data.length == undefined) {
+      array_name = 'data.todo';
+      eval(array_name).guid = eval(array_name).id;
+      result.push(eval(array_name));
+    } else {
+      for(var i=0; i<data.length; i++) {
+        array_name = 'data[i].todo';
+        eval(array_name).guid = eval(array_name).id;
+        result.push(eval(array_name));
+      }
+    }
+    
+    return result;
+  } 
+});
+
+Todos.Todo = SC.Record.extend({
+  primaryKey: 'id',
   title: null,
   isDone: false
 });
+
+Todos.QUERY_ALL_TODOS = SC.Query.local(Todos.Todo)
 
 Todos.todoListController = SC.ArrayController.create({
   content: [],
@@ -17,6 +44,10 @@ Todos.todoListController = SC.ArrayController.create({
   createTodo: function(title) {
     var todo = Todos.Todo.create({ title: title });
     this.pushObject(todo);
+  },
+  
+  setTodos: function(todos) {
+    this.set('content', todos);
   },
 
   remaining: function() {
@@ -54,6 +85,13 @@ Todos.clearCompletedView = SC.TemplateView.create({
   }
 });
 
+Todos.testView = SC.TemplateView.create({
+  mouseUp: function() {
+    var todos = Todos.store.find(Todos.QUERY_ALL_TODOS);
+    Todos.todoListController.setTodos(todos);
+  }
+});
+
 Todos.todoListView = SC.TemplateCollectionView.create({
   contentBinding: 'Todos.todoListController'
 });
@@ -75,6 +113,75 @@ Todos.statsView = SC.TemplateView.create({
 Todos.markAllDoneView = SC.TemplateView.create(SC.CheckboxSupport, {
   valueBinding: 'Todos.todoListController.allAreDone'
 });
+
+
+Todos.DataSource = SC.DataSource.extend({
+  fetch: function(store, query) {
+    if (query === Todos.QUERY_ALL_TODOS) {
+      SC.Request.getUrl('/todos.json')
+        .json()
+        .notify(this, 'fetchDidComplete', store, query)
+        .send();
+        
+      return YES;
+    }
+
+    return NO;
+  },
+  
+  fetchDidComplete: function(response, store, query) {
+    if(SC.ok(response)) {      
+      var recordType = query.get('recordType');
+      var records = Todos.TodoJSONProxy.normalize_json(response.get('body'));
+
+      store.loadRecords(recordType, records);
+      store.dataSourceDidFetchQuery(query);
+    } else {
+      alert('Error');
+      // Tell the store that your server returned an error
+      store.dataSourceDidErrorQuery(query, response);
+    }
+  },
+
+  // ..........................................................
+  // RECORD SUPPORT
+  // 
+  
+  retrieveRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to retrieve an individual record's contents
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  createRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit new records to the data source.
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  updateRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit modified record to the data source
+    // call store.dataSourceDidComplete(storeKey) when done.
+
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  destroyRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to destroy records on the data source.
+    // call store.dataSourceDidDestroy(storeKey) when done
+    
+    return NO ; // return YES if you handled the storeKey
+  }
+  
+}) ;
+
+
 
 jQuery(document).ready(function() {
   Todos.mainPane = SC.TemplatePane.append({
